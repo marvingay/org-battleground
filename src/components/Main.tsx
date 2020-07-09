c263e00565368255f367dbcdd08192cfb27c42af
Marvin Gay <MarvinJGay@protonmail.com>
2020-07-09 13:49:30 -0400

    Change: Home -> Main

---
+++ src/components/Main.tsx
@@ -0,0 +1,29 @@
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
+import { Grid } from '@material-ui/core';
+import { Switch, Route } from 'react-router-dom';
+import Announcements from './Announcements';
+import TopNavBar from './TopNavBar';
+import DisplayNameModal from './DisplayNameModal';
+import Test from './Test';
+
+const Main: React.FC = () => {
+  const { state } = useContext(GlobalContext);
+
+  return (
+    <Grid className='Home' container>
+      <TopNavBar />
+      {state.showDisplayForm && <DisplayNameModal />}
+      <Switch>
+        <Route exact path={'/announcements'}>
+          <Announcements />
+        </Route>
+        <Route path={'/'}>
+          <Test />
+        </Route>
+      </Switch>
+    </Grid>
+  );
+};
+
+export default Main;
